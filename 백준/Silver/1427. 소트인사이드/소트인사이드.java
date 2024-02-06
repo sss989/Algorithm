afdfa4621801edf2ac7680bc8fe433d8f673a12d
SOYEON <82402620+sss989@users.noreply.github.com>
2024-02-06 19:24:55 +0900

    [Silver V] Title: 소트인사이드, Time: 104 ms, Memory: 12884 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+
+        char[] charArray = str.toCharArray();
+
+        Arrays.sort(charArray);
+
+        for (int i = charArray.length - 1; i >= 0; i--) {
+            System.out.print(charArray[i]);
+        }
+    }
+}
