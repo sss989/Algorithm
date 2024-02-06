21347c68618e8f5a6916c43ee282da2af773b0f8
SOYEON <82402620+sss989@users.noreply.github.com>
2024-02-06 10:43:40 +0900

    [Bronze III] Title: 오븐 시계, Time: 152 ms, Memory: 16036 KB -BaekjoonHub

---
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(br.readLine());
+
+        int bcTotal = b + c;
+        
+        int h = (a + bcTotal / 60) % 24;
+        int m = bcTotal % 60;
+
+        System.out.println(h + " " + m);
+    }
+}
