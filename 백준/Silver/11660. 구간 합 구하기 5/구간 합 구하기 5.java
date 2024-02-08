aaae490dc7a0ca9e3bae45ca2183cb323ab742a8
SOYEON <82402620+sss989@users.noreply.github.com>
2024-02-08 14:37:58 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1724 ms, Memory: 308840 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -1,19 +1,22 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = sc.nextInt();
         int M = sc.nextInt();
         int[][] arr = new int[N + 1][N + 1];
-        int result =0;
 
         for (int i = 1; i <= N; i++)
             for (int j = 1; j <= N; j++)
                 arr[i][j] = sc.nextInt();
 
-        // acc에 arr배열 누적 합 저장
+        // acc에 arr 배열의 누적 합 저장
         int[][] acc = new int[N + 1][N + 1];
         for (int i = 1; i <= N; i++)
             for (int j = 1; j <= N; j++)
@@ -24,9 +27,15 @@ public class Main {
             int y1 = sc.nextInt();
             int x2 = sc.nextInt();
             int y2 = sc.nextInt();
-            //구간에 대한 누적합을 계산 -> result 저장
-            result = acc[x2][y2] - acc[x1 - 1][y2] - acc[x2][y1 - 1] + acc[x1 - 1][y1 - 1];
-            System.out.println(result);
+            
+            // 구간에 대한 누적합을 계산 -> result 저장
+            int result = acc[x2][y2] - acc[x1 - 1][y2] - acc[x2][y1 - 1] + acc[x1 - 1][y1 - 1];
+
+            // 결과를 BufferedWriter를 통해 출력
+            bw.write(result + "\n");
         }
+
+        bw.flush();
+        bw.close();
     }
 }
