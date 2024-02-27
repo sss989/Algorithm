c618032cce449acb7bcf5b5d577c161daf3a0b6f
SOYEON <82402620+sss989@users.noreply.github.com>
2024-02-27 21:21:31 +0900

    [Silver IV] Title: 듣보잡, Time: 832 ms, Memory: 61516 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.java
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -7,20 +7,28 @@ public class Main {
         int N = sc.nextInt();
         int M = sc.nextInt();
 
-        String[] unheard = new String[N];
+        // 듣도 못한 사람 명단을 저장할 배열 a
+        String[] a = new String[N];
         for (int i = 0; i < N; i++)
-            unheard[i] = sc.next();
-        Arrays.sort(unheard);
+            a[i] = sc.next();
+        
+        Arrays.sort(a);
 
-        List<String> result = new ArrayList<>();
+        // 듣보잡 명단 저장할 리스트
+        List<String> resultList = new ArrayList<>();
+
+
         for (int i = 0; i < M; i++) {
-            String name = sc.next();
-            if (Arrays.binarySearch(unheard, name) >= 0)
-                result.add(name);
+            String b = sc.next(); 
+            // 이분 탐색을 이용하여 a에 있는지 확인 후 참이면 리스트에 b추가
+            if (Arrays.binarySearch(a, b) >= 0)
+                resultList.add(b);
         }
 
-        result.sort(Comparator.naturalOrder());
-        System.out.println(result.size());
-        result.forEach(System.out::println);
+        // 결과 명단을 사전순으로 정렬
+        resultList.sort(Comparator.naturalOrder());
+        
+        System.out.println(resultList.size());
+        resultList.forEach(System.out::println);
     }
 }
