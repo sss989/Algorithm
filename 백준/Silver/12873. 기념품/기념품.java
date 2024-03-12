faca47ae43378693e0e634717fd79864df14f00e
SOYEON <82402620+sss989@users.noreply.github.com>
2024-03-13 00:34:24 +0900

    Update 기념품.java

--- 백준/Silver/12873. 기념품/기념품.java
+++ 백준/Silver/12873. 기념품/기념품.java
@@ -1,27 +1,31 @@
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int N = scanner.nextInt();
-        Deque<Integer> dq = new ArrayDeque<>();
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
 
-        for (int i = 1; i <= N; i++) {
-            dq.addLast(i);
-        }
+        // N까지의 티셔츠 번호 큐에 추가
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 1; i <= N; i++)
+            q.offer(i);
 
-        for (int i = 1; i <= N - 1; i++) {
-            long p = (long) Math.pow(i, 3) - 1;
-            p %= (N - i + 1);
+        int t = 1;
+        // 큐에 한명 남을 때까지 계속 반복
+        while (q.size() > 1) {
+            // 현재 단계t에서 제외될 참가자 인덱스 계산
+            long index = (long) Math.pow(t, 3) - 1;
+            index %= (q.size());
 
-            while (p-- > 0) {
-                dq.addLast(dq.pollFirst());
+            // 인덱스만큼 참가자 시계방향으로 이동
+            while (index-- > 0) {
+                q.offer(q.poll());
             }
-            dq.pollFirst();
+            q.poll(); // 제외될 참가자 큐에서 제거
+            t++; //다음단계
         }
-
-        System.out.println(dq.peekFirst());
+        System.out.println(q.poll());
     }
 }
