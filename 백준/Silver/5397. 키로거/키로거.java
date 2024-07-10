c4707c97fdc91e67395873c9704044d6d011bc69
SOYEON <82402620+sss989@users.noreply.github.com>
2024-07-11 01:49:31 +0900

    [Silver II] Title: 키로거, Time: 832 ms, Memory: 255172 KB -BaekjoonHub

--- 백준/Silver/5397. 키로거/키로거.java
+++ 백준/Silver/5397. 키로거/키로거.java
@@ -1,54 +1,35 @@
-import java.util.LinkedList;
-import java.util.Scanner;
+import java.util.*;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        int T = scanner.nextInt();
-
-        for (int t = 0; t < T; t++) {
-            String input = scanner.next();
-            String result = findPw(input);  // findPw 함수로 비밀번호를 계산
-            System.out.println(result);
-        }
-    }
-
-    // 주어진 키를 처리하여 비밀번호를 찾는 함수
-    private static String findPw(String input) {
-        LinkedList<Character> password = new LinkedList<>();  // 비밀번호를 저장하는 리스트
-        int cursor = 0;  // 커서의 위치를 나타내는 변수
-
-        for (char key : input.toCharArray()) {
-            if (key == '<') {
-                // 커서를 왼쪽으로 이동 (커서 위치가 0보다 클 때만)
-                if (cursor > 0) {
-                    cursor--;
+class Main
+{
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            char[] input = sc.next().toCharArray();
+            Deque<Character> beforCursor = new ArrayDeque<>();
+            Deque<Character> afterCursor = new ArrayDeque<>();
+            for (char cmd : input) {
+                if (cmd == '-') {
+                    beforCursor.pollLast();
                 }
-            } else if (key == '>') {
-                // 커서를 오른쪽으로 이동 (커서 위치가 리스트 크기보다 작을 때만)
-                if (cursor < password.size()) {
-                    cursor++;
+                else if (cmd == '<') {
+                    if (!beforCursor.isEmpty())
+                        afterCursor.offerLast(beforCursor.pollLast());
                 }
-            } else if (key == '-') {
-                // 백스페이스: 커서가 0보다 클 때, 왼쪽에 있는 문자를 삭제하고 커서를 왼쪽으로 이동
-                if (cursor > 0) {
-                    password.remove(cursor - 1);
-                    cursor--;
+                else if (cmd == '>') {
+                    if (!afterCursor.isEmpty())
+                        beforCursor.offerLast(afterCursor.pollLast());
                 }
-            } else {
-                // 그 외의 문자: 현재 커서 위치에 문자를 추가하고, 커서를 오른쪽으로 이동
-                password.add(cursor, key);
-                cursor++;
+                else {
+                    beforCursor.offerLast(cmd);
+                }
             }
-        }
 
-        // 비밀번호를 문자열로 변환하여 반환
-        StringBuilder result = new StringBuilder();
-        for (char c : password) {
-            result.append(c);
+            StringBuilder sb = new StringBuilder();
+            while (!beforCursor.isEmpty()) sb.append(beforCursor.pollFirst());
+            while (!afterCursor.isEmpty()) sb.append(afterCursor.pollLast());
+            System.out.println(sb);
         }
-
-        return result.toString();
     }
 }
